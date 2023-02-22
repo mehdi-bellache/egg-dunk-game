6d3e44e7f89cb40ce7ebc607ca96883b08b93ba4
Jean-Christophe <>
2023-02-22 10:16:50 +0100

    first update 22-23

--- tp3/webpack.config.js
+++ tp3/webpack.config.js
@@ -1,101 +1,110 @@
 const path = require('path');
 const webpack = require('webpack');
-
 const TerserPlugin = require('terser-webpack-plugin');
-const CopyPlugin = require("copy-webpack-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const PRODUCTION = false;
+const DIST_FOLDER = 'dist';
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src', 'scripts', 'main.js'),
+   entry: path.resolve(__dirname, 'src', 'scripts', 'main.js'),
 
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'scripts/bundle.js'
-  },
+   output: {
+      path: path.resolve(__dirname, DIST_FOLDER),
+      filename: 'scripts/bundle.js'
+   },
 
-  mode :  (PRODUCTION ? 'production' : 'development'),
-  devtool : (PRODUCTION ? undefined : 'eval-source-map'),
+   mode: (PRODUCTION ? 'production' : 'development'),
+   devtool: (PRODUCTION ? undefined : 'eval-source-map'),
 
-  devServer: {
+   devServer: {
       static: {
-	       publicPath: path.resolve(__dirname, 'dist'),
-	       watch : true
+         publicPath: path.resolve(__dirname, DIST_FOLDER),
+         watch: true
       },
-      host : 'localhost',
-      port : 9000,
-      open : true
-  },
+      host: 'localhost',
+      port: 9000,
+      open: true
+   },
 
-  module: {
-    rules: [
-      {
-        test: /\.(m?js$|jsx)/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|jpg|gif)/i,
-        use: {
-          loader: 'file-loader',
-          options: {
-            name : '[name].[ext]',
-            outputPath : 'images'
-          }
-        }
-      }
-    ]
-  },
+   module: {
+      rules: [
+         {
+            test: /\.(m?js$|jsx)/,
+            exclude: /(node_modules)/,
+            use: {
+               loader: 'babel-loader',
+               options: {
+                  presets: [
+                     '@babel/preset-env',
+                     '@babel/preset-react'
+                  ],
+                  plugins: [
+                     '@babel/transform-runtime'
+                  ]
+               }
+            }
+         },
+         {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+         },
+         {
+            test: /\.(png|jpg|gif)/i,
+            use: {
+               loader: 'file-loader',
+               options: {
+                  name: '[name][ext]',
+                  outputPath: 'images'
+               }
+            }
+         }
+      ]
+   },
 
-  plugins: [
+   plugins: [
       new webpack.ProgressPlugin(),
       new HtmlWebpackPlugin({
-	       template: path.resolve(__dirname,'src', 'index.html'),
-	        filename: './index.html',
-	        hash: true,
+         template: path.resolve(__dirname, 'src', 'index.html'),
+         filename: './index.html',
+         hash: true,
       }),
       new CopyPlugin({
-          patterns: [
+         patterns: [
             {
-              context: path.resolve(__dirname, 'src', 'html'),
-              from: "**/*.html",
-              to:  'html',
-              noErrorOnMissing: true,
-              globOptions: { }
+               context: path.resolve(__dirname, 'src', 'html'),
+               from: "**/*.html",
+               to: 'html',
+               noErrorOnMissing: true,
+               globOptions: {}
             },
             {
-              context: path.resolve(__dirname, 'src','images'),
-              from: '**/*',
-              to:  'images/[name][ext]',
-              noErrorOnMissing: true,
+               context: path.resolve(__dirname, 'src', 'images'),
+               from: '**/*',
+               to: 'images/[name][ext]',
+               noErrorOnMissing: true,
             },
             {
-             context: path.resolve(__dirname, 'src', 'style'),
-             from: '**/*.css',
-             to:  'style/[name][ext]',
-	           noErrorOnMissing: true,
-           },
+               context: path.resolve(__dirname, 'src', 'style'),
+               from: '**/*.css',
+               to: 'style/[name][ext]',
+               noErrorOnMissing: true,
+            },
          ]
-       }),
-     ],
+      }),
+   ],
 
 
-  // gestion de bibliothèques externes à exclure du bundle, ici cas de React
-  externals : {
-    react: 'React',
-    reactdom: 'ReactDom'
-  },
+   // gestion de bibliothèques externes à exclure du bundle, ici cas de React
+   externals: {
+      react: 'React',
+      'react-dom/client': 'ReactDOM'
+   },
 
 
-  optimization: {
-    minimize: true,
-    minimizer: [new TerserPlugin()]
-  }
+   optimization: {
+      minimize: true,
+      minimizer: [new TerserPlugin()]
+   }
 }
