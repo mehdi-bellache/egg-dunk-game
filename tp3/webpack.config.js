2908bf2dec80ed6a3fff693174ad731a1747fba5
Jean-Christophe <>
2023-03-01 15:19:18 +0100

    update webpack config

--- tp3/webpack.config.js
+++ tp3/webpack.config.js
@@ -56,7 +56,7 @@ module.exports = {
             use: {
                loader: 'file-loader',
                options: {
-                  name: '[name][ext]',
+                  name: '[name].[ext]',
                   outputPath: 'images'
                }
             }
