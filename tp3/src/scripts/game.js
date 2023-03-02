033572ef90f9bedeb14a0dfa84146dd03840af69
Jean-Christophe <>
2023-03-02 13:52:40 +0100

    update tir à l'arc

--- tp3/src/scripts/game.js
+++ tp3/src/scripts/game.js
@@ -1,6 +1,18 @@
 
 export default class Game {
 
+   #canvas;
+   // à compléter
+
+   constructor(canvas) {
+      this.#canvas = canvas;
+      // à compléter
+   }
+
+   /** donne accès au canvas correspondant à la zone de jeu */
+   get canvas() {
+      return this.#canvas;
+   }
 }
 
 
